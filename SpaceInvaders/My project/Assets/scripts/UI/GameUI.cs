6c4566352453d5f34603c7494b1f508623063bf2
ChaiRexLin <143537435+ChaiRexLin@users.noreply.github.com>
2023-08-31 10:45:57 -0600

    Commit

--- SpaceInvaders/My project/Assets/scripts/UI/GameUI.cs
+++ SpaceInvaders/My project/Assets/scripts/UI/GameUI.cs
@@ -7,7 +7,7 @@ public class GameUI : MonoBehaviour {
 
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private TMP_Text livesText;
-
+    [SerializeField] private TMP_Text pressSpaceText;
 
     private void Start() {
         scoreText.text = "SCORE " + ScoreManager.Instance.GetScore();
@@ -15,6 +15,22 @@ public class GameUI : MonoBehaviour {
 
         ScoreManager.Instance.OnScoreChange += ScoreManager_OnScoreChange;
         Dome.Instance.OnLivesChange += Dome_OnLivesChange;
+        GameInput.Instance.OnAction += GameInput_OnAction;
+        GameStateManager.Instance.OnStateChanged += GameStateManager_OnStateChanged;
+
+        pressSpaceText.gameObject.SetActive(false);
+    }
+
+    private void GameStateManager_OnStateChanged(object sender, System.EventArgs e) {
+        if (GameStateManager.Instance.IsGamePlaying()) {
+            pressSpaceText.gameObject.SetActive(true);
+        }
+    }
+
+    private void GameInput_OnAction(object sender, System.EventArgs e) {
+        if (GameStateManager.Instance.IsGamePlaying()) {
+            pressSpaceText.gameObject.SetActive(false);
+        }
     }
 
     private void ScoreManager_OnScoreChange(object sender, System.EventArgs e) {
