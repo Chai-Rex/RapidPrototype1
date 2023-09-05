b6f0bb6afcf2019d8d4969a40efe98e1cd3a940c
CarolineeeeeDev <LilScorpionC@gmail.com>
2023-09-04 22:06:06 -0600

    Code Modified
    1. Remove all "Find game object"
    2. Add maxMonsterAmount control

--- MergedPoject/My project/Assets/scripts/Enemies/SpawnerB.cs
+++ MergedPoject/My project/Assets/scripts/Enemies/SpawnerB.cs
@@ -13,32 +13,64 @@ public class SpawnerB : MonoBehaviour
     public GameObject[] Pos;
 
     [SerializeField] private float intervalBetweenEnemy2 = 30.0f;
+    [SerializeField] private float currentTimeWaiting = 0f;
+    [SerializeField] private int monsterMaxAmount = 0;
+
+    public int currentMonsterAmount = 0;
+    static Vector3 pos11, pos12, pos13, pos14, pos21, pos22, pos23, pos24, pos31, pos32, pos33, pos34, pos41, pos42, pos43, pos44 = new Vector3();
+    private Vector3[] pos1 = { pos11, pos12, pos13, pos14, pos21, pos22, pos23, pos24, pos31, pos32, pos33, pos34, pos41, pos42, pos43, pos44 };
+    private Vector3[] pos2 = { pos21, pos22, pos23, pos24, pos31, pos32, pos33, pos34, pos41, pos42, pos43, pos44, pos11, pos12, pos13, pos14 };
+
     void Start()
     {
-        InvokeRepeating("spawnEnemy2", 0f, intervalBetweenEnemy2);
+        for(int i = 0; i < 12; i++)
+        {
+            pos1[i] = Pos[i].transform.position;
+            pos2[i] = Pos[i + 4].transform.position;
+        }
+        for (int i = 12; i < 16; i++)
+        {
+            pos1[i] = Pos[i].transform.position;
+            pos2[i] = Pos[i - 12].transform.position;
+        }
+        InvokeRepeating("spawnEnemy2", currentTimeWaiting, intervalBetweenEnemy2);
 
     }
 
     public void spawnEnemy2()
     {
-        System.Random random = new System.Random();
-        int randomNumber = random.Next(1, 100);
-        switch (randomNumber % 2)
+        if (currentMonsterAmount < monsterMaxAmount)
         {
-            case 0://left side
-                {
-                    GameObject monster = Instantiate(Object, generatePosLeft, Quaternion.identity);
-                    monster.GetComponent<ExtraEnemy2>().direction = 0;
-                    break;
-                }
-            case 1://right side
-                {
-                    GameObject monster = Instantiate(Object, generatePosRight, Quaternion.identity);
-                    monster.GetComponent<ExtraEnemy2>().direction = 1;
-                    break;
-                }
+            System.Random random = new System.Random();
+            int randomNumber = random.Next(1, 3);
+            switch (randomNumber % 2)
+            {
+                case 0://left side
+                    {
+                        GameObject monster = Instantiate(Object, generatePosLeft, Quaternion.identity);
+                        monster.transform.parent = this.transform;
+                        currentMonsterAmount += 1;
+                        ExtraEnemy2 extra_enemy2 = monster.GetComponent<ExtraEnemy2>();
+                        extra_enemy2.direction = 0;
+                        extra_enemy2.pos1 = pos1;
+                        extra_enemy2.pos2 = pos2;
+                        break;
+                    }
+                case 1://right side
+                    {
+                        GameObject monster = Instantiate(Object, generatePosRight, Quaternion.identity);
+                        monster.transform.parent = this.transform;
+                        currentMonsterAmount += 1;
+                        ExtraEnemy2 extra_enemy2 = monster.GetComponent<ExtraEnemy2>();
+                        extra_enemy2.direction = 1;
+                        extra_enemy2.pos1 = pos1;
+                        extra_enemy2.pos2 = pos2;
+                        break;
+                    }
 
+            }
         }
+        
 
     }
 
